d46e9924a4853eb710d6a89b4a1ff1a1b1002fa7
gorkemsavran <gorkemsavran@posta.mu.edu.tr>
2021-01-28 15:03:53 +0300

    mjpeg added

--- RaspberryVideoReceiver.py
+++ RaspberryVideoReceiver.py
@@ -6,16 +6,23 @@ import threading
 
 
 class RaspberryVideoReceiver(threading.Thread):
-    def __init__(self, address=None, port=None):
+    def __init__(self, address:str="192.168.0.48", port:int=5600, isGstreamer:bool=False):
         threading.Thread.__init__(self)
-        if address and port:
+        self.cap:cv2.VideoCapture = None
+        if address and port and not isGstreamer:
+            config = f"http://{address}:{port}/stream.mjpg"
+            self.cap = cv2.VideoCapture(config)
+        elif address and port and isGstreamer:
             config = f"udpsrc address={address} port={port} ! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! avdec_h264 ! decodebin ! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert ! appsink"
+            self.cap = cv2.VideoCapture(config, cv2.CAP_GSTREAMER)
         else:
             config = 'videotestsrc ! decodebin! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert ! appsink'
-        self.cap = cv2.VideoCapture(config, cv2.CAP_GSTREAMER)
+            self.cap = cv2.VideoCapture(config, cv2.CAP_GSTREAMER)
         if not self.cap.isOpened():
             raise Exception("Cap is not opened")
         self.__frame = None
+        self.daemon = True
+        self.start()
 
     def run(self):
         while True:
--- main.py
+++ main.py
@@ -6,33 +6,23 @@ from StitcherProcess import StitcherProcess
 import time
 
 def main():
-    # queue = Queue()
-    # out_queue = Queue()
-    video = RaspberryVideoReceiver(address="192.168.0.20", port=5600)
-    video.daemon = True
-    video.start()
-    video = RaspberryVideoReceiver(address="192.168.0.20", port=5600)
-    video.daemon = True
-    video.start()
-    # video1 = RaspberryVideoReceiver(address="192.168.3.1", port=5604)
+    # send_queue = Queue()
+    # get_queue = Queue()
+    camera_receivers = [RaspberryVideoReceiver(address=f"192.168.0.{47 + i}", port=5600) for i in range(2)]
     # stitcher = cv2.createStitcher(True) if imutils.is_cv3() else cv2.Stitcher_create()
 
-    # print(cv2.getBuildInformation())
-    # for i in range(8):
     # stitcher_process = StitcherProcess(queue=queue, out_queue=out_queue)
     # stitcher_process.daemon = True
     # stitcher_process.start()
 
-    sum = 0
-    counter = 0
     while True:
-        start = time.time()
 
-        frame = video.frame()
-        # frame1 = video1.frame()
-        if frame is not None:
-            cv2.imshow("frame", frame)
-            # try:
+        frames = [receiver.frame() for receiver in camera_receivers]
+        # while None in frames:
+        #     frames = [receiver.frame() for receiver in camera_receivers]
+
+        [cv2.imshow(f"frame{idx}", frame) for idx, frame in enumerate(frames) if frame is not None]
+        # try:
             #     (status, stitched) = stitcher.stitch([frame, frame1])
             #     if status == 0:
             #         cv2.imshow("stitched", stitched)
@@ -61,30 +51,30 @@ def main():
             #     print("Finish: ", elapsed)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-import serial
-import tkinter
-from tkinter import Label, StringVar, RAISED
-import threading
-from RadioSerialConnection.RadioSerialConnection import RadioSerialConnection
-
-
-window = tkinter.Tk()
-derece0 = StringVar()
-derece1 = StringVar()
-derece0.set("0")
-derece1.set("0")
-window.title("Test")
-l0 = Label(window, textvariable=derece0)
-l1 = Label(window, textvariable=derece1)
-l0.pack()
-l1.pack()
-window.geometry("400x200")
-
-
-def main2():
-    r = RadioSerialConnection(derece0,derece1)
-    window.mainloop()
-
+#
+# import serial
+# import tkinter
+# from tkinter import Label, StringVar, RAISED
+# import threading
+# from RadioSerialConnection.RadioSerialConnection import RadioSerialConnection
+#
+#
+# window = tkinter.Tk()
+# derece0 = StringVar()
+# derece1 = StringVar()
+# derece0.set("0")
+# derece1.set("0")
+# window.title("Test")
+# l0 = Label(window, textvariable=derece0)
+# l1 = Label(window, textvariable=derece1)
+# l0.pack()
+# l1.pack()
+# window.geometry("400x200")
+#
+#
+# def main2():
+#     r = RadioSerialConnection(derece0,derece1)
+#     window.mainloop()
+#
 if __name__ == "__main__":
-    main2()
+    main()
